ebc270be8a753fcd21b8e6572ca6e04db4fa6427
Emily <emilexxxxx@gmail.com>
2024-04-08 21:46:54 +0200

    ready

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SmartContract
+
+Usage:
+
+1. Upload `poetry_publisher.sol` to [Remix](https://remix.ethereum.org/)
+2. Compile and deploy it 
---
+++ poetry_publisher.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract PoetryPublisher is ERC721 {
+    uint256 public nextTokenId;
+    address public admin;
+
+    constructor() ERC721("PoetryNFT", "PNFT") {
+        admin = msg.sender;
+    }
+
+    struct Poetry {
+        address author;
+        string content;
+        uint256 tokenId;
+    }
+
+    mapping(uint256 => Poetry) public poems;
+
+    event PoetryPublished(address indexed author, uint256 indexed tokenId);
+
+    function publishPoetry(string memory _content) external {
+        uint256 tokenId = nextTokenId;
+        _safeMint(msg.sender, tokenId);
+        poems[tokenId] = Poetry(msg.sender, _content, tokenId);
+        nextTokenId++;
+        emit PoetryPublished(msg.sender, tokenId);
+    }
+
+    function returnNFT(uint256 _tokenId) external {
+        require(msg.sender == poems[_tokenId].author, "Only author can return NFT");
+        _burn(_tokenId);
+        delete poems[_tokenId];
+    }
+}
